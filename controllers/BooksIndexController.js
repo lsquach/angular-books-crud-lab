09d10bd01f70f584b633e3f8a5e5dd14044da0cc
lsquach <lsquach@gmail.com>
2016-04-17 21:28:36 -0700

    completed lab with search and add book

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -13,4 +13,17 @@ function BooksIndexController( $http ) {
   }, function errorCallback(response) {
     console.log('There was an error getting the data', response);
   });
+
+  vm.createBook = function () {
+    $http({
+      method: 'POST',
+      url: 'https://super-crud.herokuapp.com/books',
+      data: vm.newBook,
+    }).then(function successCallback(response) {
+      vm.books.push(response.data);
+      vm.newBook = {};
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+  };
 }
